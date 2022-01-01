ea0858b34385e70171d7ac167f16967e0dd2d6db
Logan Davis <Ldavis96@live.com>
2022-01-01 15:14:04 -0500

    9.4.6 completion

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generatePage = require('./src/page.template.js');
+const generatePage = require('./src/page.template');
 
 const promptUser = () => {
     return inquirer.prompt([{
@@ -130,18 +130,11 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
-        // const fs = require('fs');
-        // const generatePage = require('./src/page.template.js');
+        const pageHTML = generatePage(portfolioData);
 
-        // const [name, github] = profileDataArgs;
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
 
-
-
-
-        // fs.writeFile('./index.html', generatePage(name, github), err => {
-        //     if (err) throw new Error(err);
-
-        //     console.log('Portfolio complete! Check out index.html to see the output!')
-        // });
+            console.log('Page created! Check out index.html in this directory to see it!');
+        });
     });
